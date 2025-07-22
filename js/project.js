d7084b2946c19cb07a182a75c2dab0ce6bb397e6
khoi817 <trinhthinhan0107@gmail.com>
2025-07-22 08:11:35 +0700

    Update project.js
    Signed-off-by: khoi817 <trinhthinhan0107@gmail.com>

--- js/project.js
+++ js/project.js
@@ -1,1 +1,21 @@
-js/project.javascript
+// Chặn chuột phải
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+// Chặn giữ tay lâu ảnh / link trên điện thoại
+document.addEventListener('touchstart', function(e) {
+  if (e.target.tagName === 'IMG' || e.target.tagName === 'A') {
+    e.preventDefault();
+  }
+}, { passive: false });
+
+// Chặn Ctrl+C hoặc copy text
+document.addEventListener('copy', function(e) {
+  e.preventDefault();
+});
+
+// Chặn kéo ảnh, text
+document.addEventListener('dragstart', function(e) {
+  e.preventDefault();
+});
